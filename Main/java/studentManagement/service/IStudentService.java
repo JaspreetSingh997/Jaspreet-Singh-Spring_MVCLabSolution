ef835fe4b0ed6d6db326c63a966e696fb6a05590
Jaspreet Singh <Jaspreet.singh251097@gmail.com>
2022-07-05 03:02:26 +0530

    Added Code

---
+++ Main/java/studentManagement/service/IStudentService.java
@@ -0,0 +1,15 @@
+package Main.java.studentManagement.service;
+
+import java.util.List;
+
+import com.studentmanagement.entity.Student;
+
+public interface IStudentService {
+	public List<Student> findAll();
+
+	public Student findById(int id);
+
+	public void save(Student student);
+
+	public void deleteById(int id);
+}
